10e9cb3b87bad3704ef1a9ff92765a2d221cb027
AnthonyMSalvati <ams6995@rit.edu>
2020-02-08 22:47:25 -0500

    created User class file

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.widget.TextView;
+
+public class User
+{
+    public User user()
+    {
+
+        return null;
+    }
+
+    private String name, password, email;
+    private int age;
+
+    TextView nameGetter =
+
+}
