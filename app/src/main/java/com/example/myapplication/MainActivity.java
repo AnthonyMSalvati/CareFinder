30a528b0b20dbbc879b555fa21460b3c40803aaa
Shane K <skirk8348@gmail.com>
2020-02-08 22:25:28 -0500

    Added jdbc support and started adding button functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
+import java.sql.*;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -13,11 +13,34 @@ import android.text.AutoText;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myapplication.ui.main.SectionsPagerAdapter;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity
 {
+Connection myConn;
+String DBurl = "jdbc:mysql://localhost:3306/people";
+String DBusername = "skirk";
+String DBpwd = "43268790";
+String DBip = "127.0.0.1";
+String DBschema = "people";
+String retrieve = "";
+
+protected String getEmployee(String filter)
+    {
+        String stmt = "select * from employees cross join people on employees.employeeID = " +
+                "people.userID where(" + filter + ")";
+        return stmt;
+    }
+
+    public void openSignUp()
+        {
+            //Intent intent = new Intent
+        }
 
 
     @Override
@@ -25,6 +48,33 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button signUpButton = findViewById(R.id.button_join);
+        signUpButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+                {
+                openSignUp();
+                }
+        });
+        TextView userNameField = findViewById(R.id.username_field);
+        TextView emailField = findViewById(R.id.user_email);
+        TextView password1Field = findViewById(R.id.password_one);
+        TextView password2Field = findViewById(R.id.password_two);
+
+
+        try
+        {
+            myConn = DriverManager.getConnection(DBurl,DBusername,DBpwd);
+            Statement myStmt = myConn.createStatement();
+//            ResultSet myRS = myStmt.executeQuery();
+        }
+        catch (java.sql.SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+
 
     }
 }
