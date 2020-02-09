dc55d78214c2c952e6a6a7f8a6c70ebc39c4f81c
AnthonyMSalvati <ams6995@rit.edu>
2020-02-09 02:17:17 -0500

    moves to login after sign up button hit

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
+
+    Button toLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,25 @@ public class SignUp extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        toLogin = findViewById(R.id.signup_but);
+        toLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                moveToLogin();
+            }
+        });
+
     }
+
+    public void moveToLogin()
+    {
+        Intent intent = new Intent(SignUp.this, Login.class);
+        startActivity(intent);
+
+    }
+
 
 }
