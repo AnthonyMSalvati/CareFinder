a23d16ec23c72205ec094041609241f48a0e360e
AnthonyMSalvati <ams6995@rit.edu>
2020-02-08 23:18:47 -0500

    User class getters made

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -1,18 +1,52 @@
 package com.example.myapplication;
 
+
 import android.widget.TextView;
 
-public class User
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class User extends AppCompatActivity
 {
+    private String name, password, email;
+
     public User user()
     {
 
         return null;
     }
 
-    private String name, password, email;
-    private int age;
 
-    TextView nameGetter =
+    public String getName()
+    {
+        TextView nameField = findViewById(R.id.username);
+        name = nameField.getText().toString();
+        return name;
+    }
+
+    public String getPassword()
+    {
+        String passOne;
+        String passTwo;
+        TextView passwordField = findViewById(R.id.password_one);
+        TextView passwordFieldTwo = findViewById(R.id.password_two);
+
+        passOne = passwordField.getText().toString();
+        passTwo = passwordFieldTwo.getText().toString();
+
+        if (passOne.equals(passTwo))
+        {
+            password = passOne;
+        }
+        return password;
+    }
+
+    public String getEmail()
+    {
+        TextView emailField = findViewById(R.id.user_email);
+        email = emailField.getText().toString();
+
+        return email;
+    }
 
 }
