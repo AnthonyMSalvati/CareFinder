7ff4960dc0d9ea08620f9a94bee616e06a85b31f
AnthonyMSalvati <ams6995@rit.edu>
2020-02-09 02:07:14 -0500

    removed unnecessary elements that were making it crash

--- app/src/main/java/com/example/myapplication/advertise.java
+++ app/src/main/java/com/example/myapplication/advertise.java
@@ -16,17 +16,7 @@ public class advertise extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.advertise);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
